34954748f3effa444b64d600fcb7273301a9f300
Alessio Buccino <alessio.buccino@alleninstitute.org>
2023-06-08 15:27:22 +0000

    Edited run_capsule.py

--- code/run_capsule.py
+++ code/run_capsule.py
@@ -41,7 +41,7 @@ scratch_folder = Path("../scratch")
 
 
 if __name__ == "__main__":
-    data_processes_folder = results_folder / "data_processes" / "spikesorting"
+    data_processes_folder = results_folder / "data_processes_spikesorting"
     data_processes_folder.mkdir(exist_ok=True, parents=True)
 
     ####### SPIKESORTING ########
